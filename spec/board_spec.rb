b930b44f56be9a538b1c9efd0f57c9594f2f3dbc
Rob Schlereth <robert.schlereth@gmail.com>
2022-07-12 12:35:03 -0500

    Test: Implement Board class tests

---
+++ spec/board_spec.rb
@@ -0,0 +1,14 @@
+require './lib/board'
+require './lib/ship'
+require './lib/cell'
+
+RSpec.describe Board do
+    it 'exists and contains cells' do
+        board = Board.new
+        expect(board.cells.length).to eq(16)
+        expect(board.cells.values).to all_be(Cell)
+    end
+end
+
+
+  
