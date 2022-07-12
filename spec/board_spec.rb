e9c1ab949623de55dfc30822363bcfac1abb8bcb
Thomas Musselman <tmusselman@gmail.com>
2022-07-12 15:43:39 -0600

    Test: create remaining tests for valid_placement? method in Board class

--- spec/board_spec.rb
+++ spec/board_spec.rb
@@ -18,16 +18,44 @@ RSpec.describe Board do
         expect(board.valid_coordinate?("A22")).to be false
     end
 
-    it 'has valid placements according to length of ship' do
+    it 'has valid placements according to lenght of ship' do
         board = Board.new
         cruiser = Ship.new("Cruiser", 3)
         submarine = Ship.new("Submarine", 2)
         expect(board.valid_placement?(cruiser, ["A1", "A2"])).to be false
         expect(board.valid_placement?(submarine, ["A2", "A3", "A4"])).to be false
         expect(board.valid_placement?(submarine, ["A2", "A3"])).to be true
-
-
     end
+
+    it 'has valid placements with consecutive cells' do
+        board = Board.new
+        cruiser = Ship.new("Cruiser", 3)
+        submarine = Ship.new("Submarine", 2)
+
+        expect(board.valid_placement?(cruiser, ["A1", "A2", "A4"])).to be false
+        expect(board.valid_placement?(submarine, ["A1", "C1"])).to be false
+        expect(board.valid_placement?(cruiser, ["A3", "A2", "A1"])).to be false
+        expect(board.valid_placement?(submarine, ["C1", "B1"])).to be false
+    end
+
+    it 'doesnt have valid placement with diagonal cells' do
+        board = Board.new
+        cruiser = Ship.new("Cruiser", 3)
+        submarine = Ship.new("Submarine", 2)
+
+        expect(board.valid_placement?(cruiser, ["A1", "B2", "C3"])).to be false
+        expect(board.valid_placement?(submarine, ["C2", "D3"])).to be false
+    end
+
+    it 'has valid placement with valid coordinates' do
+        board = Board.new
+        cruiser = Ship.new("Cruiser", 3)
+        submarine = Ship.new("Submarine", 2)
+
+        expect(board.valid_placement?(submarine, ["A1", "A2"])).to be true
+        expect(board.valid_placement?(cruiser, ["B1", "C1", "D1"])).to be true
+    end
+
 end
 
 
