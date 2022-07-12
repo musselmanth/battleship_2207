43c67152bff9cd3b3eb12d747f903b4e347a8a32
Thomas Musselman <tmusselman@gmail.com>
2022-07-12 11:51:15 -0600

    Feat: Implement Board Class initialization

--- lib/board.rb
+++ lib/board.rb
@@ -1,2 +1,17 @@
+require './lib/cell'
+require './lib/ship'
+
 class Board
+
+    attr_reader :cells
+
+    def initialize
+        @cells = {}
+        ("A".."D").each do |row|
+            ("1".."4").each do |column|
+                @cells[row + column] = Cell.new(row + column)
+            end
+        end
+    end
+    
 end
--- spec/board_spec.rb
+++ spec/board_spec.rb
@@ -6,7 +6,7 @@ RSpec.describe Board do
     it 'exists and contains cells' do
         board = Board.new
         expect(board.cells.length).to eq(16)
-        expect(board.cells.values).to all_be(Cell)
+        expect(board.cells.values).to all(be_a(Cell))
     end
 end
 
