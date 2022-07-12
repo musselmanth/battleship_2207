b68eb047075bdd868eeb354eeef4d2595a119442
Thomas Musselman <tmusselman@gmail.com>
2022-07-12 11:54:16 -0600

    Test: Create test for Board class valid_coordinate? method

--- spec/board_spec.rb
+++ spec/board_spec.rb
@@ -8,6 +8,15 @@ RSpec.describe Board do
         expect(board.cells.length).to eq(16)
         expect(board.cells.values).to all(be_a(Cell))
     end
+
+    it 'has valid coordinates' do
+        board = Board.new
+        expect(board.valid_coordinate?("A1")).to be true
+        expect(board.valid_coordinate?("D4")).to be true
+        expect(board.valid_coordinate?("A5")).to be false
+        expect(board.valid_coordinate?("E1")).to be false
+        expect(board.valid_coordinate?("A22")).to be false
+    end
 end
 
 
