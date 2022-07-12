5f391cb359af63ed6536b2eb9253d10dd7654cfc
Thomas Musselman <tmusselman@gmail.com>
2022-07-11 20:28:37 -0600

    Feature: implement further cell methods

--- lib/cell.rb
+++ lib/cell.rb
@@ -5,6 +5,7 @@ class Cell
     def initialize(coordinate)
         @coordinate = coordinate
         @ship = nil
+        @fired_upon = false
     end
 
     def empty?
@@ -14,4 +15,16 @@ class Cell
     def place_ship(ship)
         @ship = ship
     end
+
+    def fired_upon?
+        @fired_upon
+    end
+
+    def fire_upon
+        if !empty?
+            @ship.hit
+            @fired_upon = true
+        end
+    end
+
 end
