612f0f173589610d3ed0c770f15b9a1f199bf88f
Thomas Musselman <tmusselman@gmail.com>
2022-07-15 12:07:54 -0600

    test: create some initial tests for computer placing ships

--- spec/computer_spec.rb
+++ spec/computer_spec.rb
@@ -1,11 +1,33 @@
 require './lib/computer'
 
 RSpec.describe Computer do
-
-  it 'exists' do
-    computer = Computer.new
-    
-    expect(computer).to be_instance_of(Computer)
+  before(:each) do
+    @computer = Computer.new
   end
+  it 'exists' do
+    expect(@computer).to be_instance_of(Computer)
+  end
+  it 'has ships' do
+    expect(@computer.ships).to all(be_instance_of(Ship))
+  end
+  it 'has a board' do
+    expect(@computer.board).to be_instance_of(Board)
+  end
+  it 'places its ships' do
+    @computer.place_ships
+    occupied_cells = @computer.board.cells.count do |coord, cell| 
+      !cell.empty?
+    end
 
+    expect(occupied_cells).to eq(5)
+  end
+  it 'places ships differently each time' do
+    @computer.place_ships
+    first_comp_init_render = @computer.board.render(true)
+    computer2 = Computer.new
+    computer2.place_ships
+    second_comp_init_render = computer2.board.render(true)
+
+    expect(first_comp_init_render).not_to eq(second_comp_init_render)
+  end
 end
